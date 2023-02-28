80539656267f1a0d7fb481dfc44e79d454b33c4d
Maxim Polovinkin <polloVinkin@yandex.ru>
2023-03-01 00:40:32 +0300

    /

--- hammer/ServiceLayer/NetworkService.swift
+++ hammer/ServiceLayer/NetworkService.swift
@@ -14,22 +14,24 @@ protocol NetworkServiceProtocol {
 
 class NetworkService: NetworkServiceProtocol {
     func getData(completion: @escaping (Result<[menuItems]?, Error>) -> Void) {
-        guard let url = URL(string: "https://api.spoonacular.com/food/menuItems/search?query=pizza&number=20&apiKey=b0912c42d2ac47ca9b8a40ec6ca11313") else { return }
-        
-        URLSession.shared.dataTask(with: url) {data, _, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
+        DispatchQueue.global().async {
+            guard let url = URL(string: "https://api.spoonacular.com/food/menuItems/search?query=pizza&number=20&apiKey=b0912c42d2ac47ca9b8a40ec6ca11313") else { return }
             
-            do{
-                let data = try JSONDecoder().decode(initial.self, from: data!)
-                completion(.success(data.menuItems))
-            } catch {
-                completion(.failure(error))
-            }
-            
-        }.resume()
+            URLSession.shared.dataTask(with: url) {data, _, error in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+                
+                do{
+                    let data = try JSONDecoder().decode(initial.self, from: data!)
+                    completion(.success(data.menuItems))
+                } catch {
+                    completion(.failure(error))
+                }
+                
+            }.resume()
+        }
     }
 }
 
